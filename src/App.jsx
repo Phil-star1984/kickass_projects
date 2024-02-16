6f38fd63a50dedc1b54e6d7d838eb5c8182a28d3
Philipp Mulfinger <info@phil-splash.de>
2024-02-16 16:38:09 +0100

    Added Meme Generator in WeekFour

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
-
+/* import WeekOne from "./components/WeekOne.jsx"; */
+/* import WeekTwo from "./components/WeekTwo.jsx"; */
 import Home from "./pages/Home.jsx";
-import WeekOne from "./components/WeekOne.jsx";
 import WeekOneTest from "./components/WeekOneTest.jsx";
-import WeekTwo from "./components/WeekTwo.jsx";
 import WeekTwoSecond from "./components/WeekTwoSecond.jsx";
 import WeekThree from "./components/WeekThree.jsx";
 import Footer from "./components/Footer.jsx";
+import WeekFour from "./components/WeekFour.jsx";
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
         <Route path="/weekone" element={<WeekOneTest />} />
         <Route path="/weektwo" element={<WeekTwoSecond />} />
         <Route path="/weekthree" element={<WeekThree />} />
+        <Route path="/weekfour" element={<WeekFour />} />
       </Routes>
       <Footer />
     </>
