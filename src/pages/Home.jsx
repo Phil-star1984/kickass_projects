b7764530708f9743621d07b7760886da5ed493c1
Philipp Mulfinger <info@phil-splash.de>
2024-03-11 20:36:54 +0100

    fix: reset homepage with all components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-/* import WeekOneTest from "../components/WeekOneTest.jsx";
+import WeekOneTest from "../components/WeekOneTest.jsx";
 import WeekTwoSecond from "../components/WeekTwoSecond.jsx";
 import WeekThree from "../components/WeekThree.jsx";
 import WeekFour from "../components/WeekFour.jsx";
 import WeekFive from "../components/WeekFive.jsx";
 import WeekSix from "../components/WeekSix.jsx";
-import WeekSeven from "../components/WeekSeven.jsx"; */
+import WeekSeven from "../components/WeekSeven.jsx";
 
 function Home() {
   return (
     <>
-      {/* <WeekOneTest />
+      <WeekOneTest />
       <WeekTwoSecond />
       <WeekThree />
       <WeekFour />
       <WeekFive />
       <WeekSix />
-      <WeekSeven /> */}
-      <h1>HELLO</h1>
+      <WeekSeven />
+      {/* <h1>HELLO</h1> */}
     </>
   );
 }
